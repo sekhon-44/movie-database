aba7bf47c99aa99ba3414655ecd039dfec8de042
Evan <eliao13@my.bcit.ca>
2022-05-16 16:10:00 -0700

    fixed search bar output error when searching sp or rip

--- src/compenents/SearchResultsContainer.js
+++ src/compenents/SearchResultsContainer.js
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
 
-function SearchResultsContainer({searchResults, error}) {
+function SearchResultsContainer({ searchResults, error }) {
+
+  {
+    searchResults.map(searchResult => {
+      console.log(searchResult.original_title)
+      console.log(searchResult.release_date)
+    }
+    )
+  }
+
   return (
-    <div className="searchResults"> 
+    <div className="search-results">
+      <ul>
         {error !== false && <li>{error}</li>}
-        {searchResults.map(searchResult => 
-            <li key={searchResult.id}>
-                <Link to={`/movie/${searchResult.id}`}>
-                {searchResult.original_title} {searchResult.release_date.substring(0, 4)}
-                </Link>
-            </li>)}
+        {searchResults.map(searchResult =>
+          <li key={searchResult.id}>
+            <Link to={`/movie/${searchResult.id}`}>
+              {searchResult.original_title} {(typeof (searchResult.release_date) !== 'undefined' && (searchResult.release_date) !== '') && `(${searchResult.release_date.substring(0, 4)})`}
+            </Link>
+          </li>)}
+      </ul>
     </div>
   )
 }
