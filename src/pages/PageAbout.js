c69c463a43d04bad3a025d885e9eb14cbbe70e6c
JaeYoung-Ahn <ooctrl88@gmail.com>
2022-05-19 10:38:27 -0700

    mobile ver. of aboutpage

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,6 +1,18 @@
+import React from 'react';
+// import AboutPage from '../compenents/AboutPage';
+
 function PageAbout() {
     return (
-        <div>PageAbout</div>
+        <div className="about-page-container">
+            <section className="about-page">
+                <h2>About MovieCentral</h2>
+                    <p>
+                        MovieCentral is an online movie database that lets users browse movies based on popularity, rating, and release date. The platform also allows registered users to keep track of movies they love by adding films to their favorites list.
+                    </p>
+                    <h3>Follow Us</h3>
+            </section>
+            
+        </div>
     )
 }
 export default PageAbout
