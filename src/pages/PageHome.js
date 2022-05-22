306fc1900592d725dce5b3c8de864c699b985a9c
sekhon-44 <sekhongaven@yahoo.com>
2022-05-21 21:55:47 -0700

    added sorting and cleaned it up

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -34,24 +34,6 @@ function PageHome({ sort }) {
         
         useEffect(() => {
             
-            // const grabMovies = async () => {
-            //     const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
-                
-            //     const data = await res.json();
-                
-            //     if(data.success === false){
-            //         moviesData !== false && setMoviesData(false);
-                    
-            //     }else{
-            //         const first12Movies = data.results.splice(0,12);
-            //         setMoviesData(first12Movies);
-            //     }
-                
-            // }
-            // console.log(moviesData)
-            // grabMovies()
-
-
             const grabMovies = async () => {
                 const res = await fetch(`https://api.themoviedb.org/3/movie/${sort}?api_key=${API_KEY}&language=en-US&page=1`)
 
