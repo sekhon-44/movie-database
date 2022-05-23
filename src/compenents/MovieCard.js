821b59e0f90fdce46cfc471b7fa2cf5693bf7413
sekhon-44 <sekhongaven@yahoo.com>
2022-05-22 19:18:20 -0700

    Revert "changed sizing of hover title"
    This reverts commit 81f108410126466ba64a9bd01016a0d68c3e33fd.

--- src/compenents/MovieCard.js
+++ src/compenents/MovieCard.js
@@ -22,7 +22,7 @@ function MovieCard({ movie }) {
           }
           {isShown && ( 
             <div className="movie-info-hover">
-                <h2>{movie.title}</h2>
+                <h3>{movie.title}</h3>
                 <p>{movie.overview}</p>
                 <Link className={'detail-btn'} to={`/movie/${movie.id}`}>More Info</Link>
             </div>
